ef1cc6734c8b07e4a97a0a01c93d2979f4f29da7
DDuowg <doducduong1007@gmail.com>
2025-02-03 16:00:48 +0700

    (update) frontend & theme update

--- premiere/src/components/VideoListComponent.tsx
+++ premiere/src/components/VideoListComponent.tsx
@@ -153,7 +153,7 @@ export default function Home() {
                       },
                     }}
                   >
-                    <Preview data={data} />
+                    <Preview key={data._id} data={data} />
                   </Box>
                 )
               )
