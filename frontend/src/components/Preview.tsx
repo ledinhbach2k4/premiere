91a0300436e623367a0ac194ee439815c005b1e6
Gia Phuc <giaphuc29082004@gmail.com>
2025-02-18 15:37:41 +0700

    gio co the an vao nguyen cai paper preview de navigate qua temple. An nut favorite se khong navigate

--- frontend/src/components/Preview.tsx
+++ frontend/src/components/Preview.tsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IVideo } from "./../interface/type";
 
-export default function Preview(props: { key: string, data: IVideo }) {
-
+export default function Preview(props: { key: string; data: IVideo }) {
   const [liked, setLiked] = useState(false);
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const [videoLoaded, setVideoLoaded] = useState(false);
+  const [hover, setHover] = useState(false);
 
   const toggleLike = () => {
     setLiked(!liked); // Toggle like state
@@ -21,7 +21,6 @@ export default function Preview(props: { key: string, data: IVideo }) {
   const navigate = useNavigate();
   return (
     <>
-      
       <Paper
         sx={{
           width: 300,
@@ -32,39 +31,53 @@ export default function Preview(props: { key: string, data: IVideo }) {
           flexDirection: "column",
           gap: 1,
           position: "relative",
-          overflow: "hidden", // Ensures content doesn't overflow
+          overflow: "hidden",
         }}
-
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={() => {
+          navigate(`/template/:${_id}`);
+        }}
       >
-        {!imageLoaded && (
-          <></>
-          // <Skeleton variant="rectangular" width={300} height={170} />
+        {!videoLoaded && (
+          <Skeleton
+            variant="rectangular"
+            animation={false}
+            width={300}
+            height={170}
+          />
         )}
-        <video
-          src={`/vidPreview/${_id}.webm`}
-          style={{
-            width: '300px',
-            height: '170px',
-            objectFit: 'cover',
-            display: imageLoaded ? 'block' : 'none', 
-          }}
-          onLoadedData ={() => {setImageLoaded(true)}}
-          onClick={() => {
-            navigate(`/template/:${ _id }`);
-          }}
-          autoPlay
-          muted // Để autoplay hoạt động trên hầu hết trình duyệt
-          onEnded={(e) => {
-            e.currentTarget.currentTime = 0; // Reset về đầu video
-            e.currentTarget.play(); // Phát lại
-          }}
-        />
+        {hover ? (
+          <video
+            src={`/vidPreview/${_id}.webm`}
+            style={{
+              width: "300px",
+              height: "170px",
+              objectFit: "cover",
+              display: videoLoaded ? "block" : "none",
+            }}
+            onLoadedData={() => {
+              setVideoLoaded(true);
+            }}
+            autoPlay
+            muted // Để autoplay hoạt động trên hầu hết trình duyệt
+            onEnded={(e) => {
+              e.currentTarget.currentTime = 0; // Reset về đầu video
+              e.currentTarget.play(); // Phát lại
+            }}
+          />
+        ) : (
+          <img></img>
+        )}
         <Typography variant="h5">{title}</Typography>
         <Typography variant="body2">{likeNum} liked </Typography>
         <Typography variant="body2">{releaseDateString} </Typography>
 
         <IconButton
-          onClick={toggleLike}
+          onClick={(e) => {
+            e.stopPropagation();
+            toggleLike();
+          }}
           sx={{
             position: "absolute",
             bottom: 8,
